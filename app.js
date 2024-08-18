defbb937c8c50a685a116c032f54a20d992c4fdf
JadhavRushiS <rsjadhav3110@gmail.com>
2024-08-18 21:34:03 +0530

    --

--- app.js
+++ app.js
@@ -11,3 +11,20 @@ console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(heading);
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "Child" }, [
+    React.createElement("h1", {}, "Nested Element 1"),
+    React.createElement("h2", {}, "Nested 1"),
+  ]),
+  React.createElement("div", { id: "Child" }, [
+    React.createElement("h1", {}, "Nested Element 2"),
+    React.createElement("h2", {}, "Nested 2"),
+  ]),
+]);
+
+//jsx
+
+const nestedElement = ReactDOM.createRoot(document.getElementById("parent"));
+
+nestedElement.render(parent);
